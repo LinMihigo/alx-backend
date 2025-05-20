753833446a8372ad3228c01ef501f00e393c1736
LinMihigo <linmihigo@gmail.com>
2025-05-20 20:17:27 +0200

    Task 0 to 1 - 1st fix

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -15,16 +15,16 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @app.route('/')
 def index():
+    """Home page
     """
-    """
-    return render_template('index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
